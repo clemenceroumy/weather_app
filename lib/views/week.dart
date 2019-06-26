288b78294a95720ef42cfa56a5ee95823c83fb75
Clemence Roumy <clemenceroumy@gmail.com>
2019-06-26 12:31:40 +0200

    Week visual structure

--- lib/views/week.dart
+++ lib/views/week.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class Week extends StatefulWidget {
@@ -7,17 +8,59 @@ class Week extends StatefulWidget {
 }
 
 class _WeekState extends State<Week> {
+  List<Expanded> generateColumn() {
+    List<Expanded> listColumn = new List<Expanded>();
+
+    for (var i = 1; i < 7; i++) {
+      listColumn.add(Expanded(
+        child: Container(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Icon(Icons.cloud_queue),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Text(
+                    'Thursday',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Text(
+                    '20°C / 33°C',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ));
+    }
+    return listColumn;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0)),
-          color: Colors.white,
-        ),
-        height: 300,
-        child: Row(
-          children: <Widget>[Text("yo!")],
-        ));
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0)),
+        color: Colors.white,
+      ),
+      height: 400,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: this.generateColumn(),
+      ),
+    );
   }
 }
